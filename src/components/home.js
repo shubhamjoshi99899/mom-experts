44f661d6acecb74bfc3e7e4a17fc386e260b3322
Shubham Joshi <83740922+shubhamjoshi99899@users.noreply.github.com>
2022-11-22 17:27:58 +0530

    added aos

--- src/components/home.js
+++ src/components/home.js
@@ -1,34 +1,49 @@
 import React, { useEffect, useState } from "react";
-import Carousel from './carousel'
-import Hero from './hero'
+import Carousel from "./carousel";
+import Hero from "./hero";
 import Features from "./features";
 import Stats from "./stats";
-import Header from './header';
+import Header from "./header";
 import Testimonials from "./testimonials";
 import Contact from "./contact";
+import AOS from "aos";
+import "aos/dist/aos.css";
 const Home = () => {
+  const [user, setUser] = useState({});
 
-  const [user, setUser] = useState({})
-
-  useEffect(()=>{
+  useEffect(() => {
     const data = localStorage.getItem("user");
     if (data) {
       setUser(JSON.parse(data));
     }
-   
-  },[])
+  }, []);
 
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div>
-      <Header user={user}/>
+      <Header user={user} />
       <Carousel user={user} />
-      <Hero />
-      <Features />
+      <div
+        data-aos="zoom-in"
+        data-aos-duration="3000" //Here you can use any of the AOS animations
+      >
+        <Hero />
+      </div>
+      <div data-aos="fade-up">
+        <Features />
+      </div>
+      <div data-aos="fade-right">
+        <Stats />
+      </div>
 
-      <Stats />
-
-      <Testimonials />
-      <Contact />
+      <div data-aos="fade-left">
+        <Testimonials />
+      </div>
+      <div data-aos="zoom-in">
+        <Contact />
+      </div>
     </div>
   );
 };
